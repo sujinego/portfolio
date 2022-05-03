354fa076ffcac5d938c5bfade06591fd9d993b9e
sujinego <sujinego3971@naver.com>
2022-05-03 19:55:00 +0900

    portfolio web

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+
+export const App = () => {
+  return (
+    <div>
+        <header />
+        <Nav />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+const Hader = () => {
+  return (
+    <div>Hader</div>
+  )
+}
+
+export default Hader

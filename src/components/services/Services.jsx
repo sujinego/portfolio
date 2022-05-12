c5184c7f8a491f9db5475622588701971af3f16e
sujinego <sujinego3971@naver.com>
2022-05-12 19:58:30 +0900

    5/12

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,16 @@ import './services.css'
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className="service">
+          
+        </article>
+      </div>
+    </section>
   )
 }
 

24a065690698af6bfa081c29fbb948ee8dae7f6a
sujinego <sujinego3971@naver.com>
2022-05-18 21:04:24 +0900

    service

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import './portfolio.css'
 
 const Portfolio = () => {
     return (
-        <section id="portfolio">portfolio</section>
+        <section id="portfolio">
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+
+            <div className="container portfolio_container">
+                <article className="portfolio_item">
+                    <div className="portfolio_item_image"></div>
+                    <h3>This is a portfolio item title</h3>
+                    <a href="http://github" className="btn" target="_blank">github</a>
+
+                </article>
+            </div>
+        </section>
     )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './services.css'
+import {BsCheck} from 'react-icons/bs'
 
 const Services = () => {
   return (
@@ -9,7 +10,85 @@ const Services = () => {
 
       <div className="container services_container">
         <article className="service">
-          
+          <div className="service_head">
+            <h3>UI/UX</h3>
+          </div>
+          <ul className="services_list">
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트1</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트2</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트3</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트4</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트5</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service_head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="services_list">
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트1</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트2</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트3</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트4</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트5</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="services_list">
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트1</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트2</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트3</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트4</p>
+            </li>
+            <li>
+              <BsCheck className="service_list_icon" />
+              <p>리스트5</p>
+            </li>
+          </ul>
         </article>
       </div>
     </section>
